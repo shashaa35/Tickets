dd14840dce480b14a6d2b7ef75cf82ad5df6ce4c
Shashank Agarwal <shasagarwal@microsoft.com>
2023-07-25 00:25:04 +0530

    adding static files, and updating paths

--- ticketSite/ticketSite/settings.py
+++ ticketSite/ticketSite/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DEBUG')
 print(os.environ.get('ALLOWED_HOSTS'))
 print(os.getenv('ALLOWED_HOSTS'))
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
 
 # Application definition
 # import smart_selects
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
